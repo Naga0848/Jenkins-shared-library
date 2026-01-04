efb6ebd0de122234d40469c2ddfb17668779bc3b
Nagashankar <nagashankar0848@gmail.com>
2026-01-04 22:46:45 +0530

    jenkins

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -113,8 +113,8 @@ def call(Map configMap){
                         withAWS(credentials: 'aws-creds', region: 'us-east-1') {
                             sh """
                                 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${APP_VERSION} .
-                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${APP_VERSION}
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${env.APP_VERSION} .
+                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${env.APP_VERSION} .
                                 #aws ecr wait image-scan-complete --repository-name ${PROJECT}/${COMPONENT} --image-id imageTag=${APP_VERSION} --region ${REGION}
                             """
                         }
